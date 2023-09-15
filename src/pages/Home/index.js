2878304e5e575b1b5be3d696741b8f66a9a6e124
Yace Sf <yacine.sf@outlook.fr>
2023-09-15 14:42:53 +0200

    Resolution du slider, date et de l'image du footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,10 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { data } = useData();
+  const dataEvents = data?.events;
+  const sortEvents = dataEvents?.sort((a, b) => (a.id) > (b.id) ? -1 : 1 )[0];
+  
   return <>
     <header>
       <Menu />
@@ -22,7 +25,7 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
+      <section id="nos-services" className="ServicesContainer">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -51,11 +54,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section id="nos-realisations" className="EventsContainer">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section id="notre-equipe" className="PeoplesContainer">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
@@ -116,13 +119,14 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
+        {sortEvents && (
         <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
+          imageSrc={sortEvents?.cover}
+          title={sortEvents?.title}
+          date={new Date(sortEvents?.date)}
           small
           label="boom"
-        />
+        />)}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
